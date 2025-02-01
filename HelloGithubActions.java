9a13b156513e5217cb7c5928f61e0c5f4fe686e5
Thurunu Mihiranga <86581450+Thurunu@users.noreply.github.com>
2025-02-02 02:17:44 +0530

    second commit

--- HelloGithubActions.java
+++ HelloGithubActions.java
@@ -8,5 +8,6 @@ public class HelloGithubActions {
     public static int generateRandomNumber(int min, int max) {
         Random random = new Random();
         return random.nextInt((max - min) + 1) + min;
+        
     }
 }
